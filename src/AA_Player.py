135d05c592320e9c2baab316cd5a0ea2bece73cf
apg203 <71585354+apg203@users.noreply.github.com>
2022-10-20 20:23:38 +0200

    update: interface, 1 player

--- src/AA_Player.py
+++ src/AA_Player.py
@@ -1,4 +1,5 @@
-import sys, pygame
+import sys
+import pygame
 import tkinter
 import math
 import tkinter.messagebox
@@ -6,11 +7,13 @@ import tkinter.filedialog
 from pygame.locals import *
 import time
 from random import randrange
+import AA_Engine
 
 SIZE_MAP = 20
 SIZE_CITY = SIZE_MAP / 2
 
 MARGEN = 5
+MARGEN_SUPERIOR = 60
 MARGEN_INFERIOR = 60
 TAM = 30
 
@@ -21,109 +24,60 @@ RED=(255, 0, 0)
 BLUE=(0, 0, 255)
 YELLOW=(255, 255, 0)
 
-class Cell():
-    def __init__(self, r, c):
-        self.row = r
-        self.column = c
-        
-    def getRow (self):
-        return self.row
-    
-    def getColumn (self):
-        return self.column
-    
-    def __eq__(self, other):
-        return self.getColumn() == other.getColumn() and self.getRow() == othergetRow()
-
-class Map():
-    def __init__(self):        
-        self.map = setMap()
-        self.height = SIZE_MAP
-        self.width = SIZE_MAP
-        
-    def getHeight (self):
-        return self.height
-    
-    def getWidth (self):
-        return self.width
-        
-    def getCell(self, y, x):
-        return self.map[y][x]
-    
-    def setCell(self, y, x, value):
-        self.map[y][x] = value
-        
-def setMap():
-    
-    # TODO: El número de alimentos no debe ser divisible entre el número
-    # de jugadores para evitar un empate, mucho menos número par
-    map = []
-    bombs = 0
-    food = 0
-    players = 0
-    value = 0
-    zeroes = 0
-    
-    for i in range(SIZE_MAP):
-        rowList = []
-        for j in range(SIZE_MAP):
-            count = randrange(20) # de 0 a 19
-            if count == 0:
-                zeroes = zeroes + 1
-            if count == 0 and players == 0 and zeroes >= 10:
-                players = players + 1
-                value = 3
-            elif count < 8:
-                bombs = bombs + 1
-                value = 0
-            elif count >= 8 and count <= 10:
-                food = food + 1
-                value = 2
-            elif count > 10:
-                value = 1
-            
-            rowList.append(value)
-        map.append(rowList)
-
-    return map
-
 def printMap(map):
-    
+
     screen.fill(BLACK)
-    for fil in range(map.getHeight()):
-        for col in range(map.getWidth()):
-            if map.getCell(fil, col) == 0:
+    for fil in range(game.map.SIZE):
+        for col in range(game.map.SIZE):
+            if game.map.getCell(fil, col) == "M":
                 pygame.draw.rect(screen, RED, [(TAM+MARGEN)*col+MARGEN, (TAM+MARGEN)*fil+MARGEN, TAM, TAM], 0)
-            if map.getCell(fil, col) == 1:
+            if game.map.getCell(fil, col) == " ":
                 pygame.draw.rect(screen, WHITE, [(TAM+MARGEN)*col+MARGEN, (TAM+MARGEN)*fil+MARGEN, TAM, TAM], 0)
-            if map.getCell(fil, col) == 2:
+            if game.map.getCell(fil, col) == "A":
                 pygame.draw.rect(screen, GREEN, [(TAM+MARGEN)*col+MARGEN, (TAM+MARGEN)*fil+MARGEN, TAM, TAM], 0)
             # El jugador de prueba
-            if map.getCell(fil, col) == 3:
+            if game.map.getCell(fil, col) == "J":
                 pygame.draw.rect(screen, BLUE, [(TAM+MARGEN)*col+MARGEN, (TAM+MARGEN)*fil+MARGEN, TAM, TAM], 0)
-        
+
 if __name__=="__main__":
-    map = Map()
+    game = AA_Engine.Game()
+    game.newPlayer("J", 4,19)
+    fromcell = AA_Engine.Cell(4,19)
     pygame.init()
     clock = pygame.time.Clock()
-    
-    width = map.getWidth()*(TAM+MARGEN) + MARGEN
-    height = MARGEN_INFERIOR + map.getHeight()*(TAM+MARGEN) + MARGEN
+
+    width = game.map.SIZE*(TAM+MARGEN) + MARGEN
+    height = MARGEN_INFERIOR + game.map.SIZE*(TAM+MARGEN) + MARGEN
     dimension = [width, height]
     screen=pygame.display.set_mode(dimension)
     pygame.display.set_caption("SDestrozo")
-    
+
     running= True
-    
+
     while running:
+        pygame.display.update()
+
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:               
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    game.move("J", fromcell, AA_Engine.Direction.N)
+                    fromcell = fromcell + AA_Engine.Direction.N
+                if event.key == pygame.K_RIGHT:
+                    game.move("J", fromcell, AA_Engine.Direction.S)
+                    fromcell = fromcell + AA_Engine.Direction.S
+                if event.key == pygame.K_UP:
+                    game.move("J", fromcell, AA_Engine.Direction.W)
+                    fromcell = fromcell + AA_Engine.Direction.W
+                if event.key == pygame.K_DOWN:
+                    game.move("J", fromcell, AA_Engine.Direction.E)
+                    fromcell = fromcell + AA_Engine.Direction.E
+                fromcell.normalize(game.map.SIZE, game.map.SIZE)
+            if event.type == pygame.QUIT:
                 running = False
-        
-        printMap(map)
-        
-        pygame.display.flip()
+
         clock.tick(40)
-    
+
+        printMap(game)
+
     pygame.quit()
-    
+    sys.exit()
