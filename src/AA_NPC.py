d0d458b8aece9ccf1cd736fb1a10ea55b73ee3ca
apg203 <71585354+apg203@users.noreply.github.com>
2022-11-11 19:29:04 +0100

    npc kafka

--- src/AA_NPC.py
+++ src/AA_NPC.py
@@ -1,123 +1,75 @@
-import sys, random, time
+import sys, random, time, pickle
+from threading import Thread
+from common_utils import MySocket
+from kafka import KafkaConsumer, KafkaProducer
 
-'''
-    PENDIENTE:
-        - Kafka: enviar cada movimiento
-'''
+ADDR_KAFKA = [sys.argv[1]+":29092"]
 
-class NPC():
-    # no le afecta la temperatura ni cambia de nivel
-    def __init__(self, alias, category):
-        self.alias = alias
-        self.pos = (-1,-1) # una cell o algo
-        # la categoría es por eso del npc que persigue y el que no
-        # categoría sería tipo 1 o 2, pero si ves que meh bórralo
-        self.category = category
-        self.__level = random.randint(8,12)
+def getDirec(cardinal):
+    tuplita = (0,0)
 
-    def __str__(self):
-        return "NPC " + self.alias + ", con nivel " + str(self.getTotalLevel())
+    if cardinal == "N":
+        tuplita = (0,-1)
+    elif cardinal == "S":
+        tuplita = (0,1)
+    elif cardinal == "W":
+        tuplita = (-1,0)
+    elif cardinal == "E":
+        tuplita = (1,0)
+    elif cardinal == "SE":
+        tuplita = (1,1)
+    elif cardinal == "SW":
+        tuplita = (-1,1)
+    elif cardinal == "NE":
+        tuplita = (1,-1)
+    elif cardinal == "NW":
+        tuplita = (-1,-1)
+    else:
+        tuplita = (0,0)
 
-    # el NPC sólo puede morir si entra en una pelea con jugadores -> (y entre npcs qué? npi)
-    def isAlive(self):
-        return self.__level != -1
+    return tuplita
 
-    def die(self):
-        self.__level = -1
-
-    # nada de cambios por temperatura, recuerda
-    def getTotalLevel(self):
-        return self.__level
-
-    def getAlias(self):
-        return self.alias
-
-    def getCategory(self):
-        return self.category
-
-    def getPos(self):
-        return self.pos
-
-def distancia_manhattan(origen,destino):
-    distancia = 0
-
-    abs_x = abs(destino.getFila()-origen.getFila())
-    abs_y = abs(destino.getCol()-origen.getCol())
-
-    distancia = abs_x + abs_y
-
-    return distancia
-
-def orientation(npc, player):
-    ori = ""
-    abs_x = abs(player.getFila()-npc.getFila())
-    abs_y = abs(player.getCol()-npc.getCol())
-
-    if abs_x == 0:
-        if abs_y > 0:
-            ori = "S"
-        elif abs_y < 0:
-            ori = "N"
-    elif abs_x < 0:
-        if abs_y > 0:
-            ori = "SW"
-        elif abs_y == 0:
-            ori = "W"
-        elif abs_y < 0:
-            ori = "NW"
-    elif abs_x > 0:
-        if abs_y > 0:
-            ori = "SE"
-        elif abs_y == 0:
-            ori = "E"
-        elif abs_y < 0:
-            ori = "NE"
-
-    return ori
-
-# de momento los NPCs pares no se mueven porque no buscan
-# mientras que los NPCs impares se mueven al azar
-def move(npc, cat):
-    direc=["N","S","W","E","NE","SW","NW","SE"]
+def funcsend(alias):
+    producer = KafkaProducer(
+        bootstrap_servers = ADDR_KAFKA,
+        value_serializer = lambda v: pickle.dumps(v)
+    )
+    i = (0,0)
+    direc = ""
     res = ""
-    d_min = 99999
-    d = 99999
-    idx = 0
-    idx_min = 0
-
-    if cat == False:
+    alive = True
+    while alive:
+        direc=["N","S","W","E","NE","SW","NW","SE"]
         random_idx = random.randint(0,7)
         res = direc[random_idx]
-        print(npc.getAlias() + " se mueve hacia el: "+ direc[random_idx])
-    else:
-        pass
-        '''
-        for p in players:
-            d = distancia_manhattan(npc.getPos(), p.getPos())
-            idx = idx + 1
-            if d < d_min:
-                d_min = d
-        idx_min = idx
+        i = getDirec(res)
+        data = {alias:i}
+        if i != (0,0):
+            producer.send('movement', value=data)
+        i = (0,0)
+        direc = ""
+        res = ""
+        time.sleep(random.uniform(0, 2))
 
-        res = orientation(npc.getPos(), players[idx_min].getPos())
-        '''
+def funcrecv():
+    consumer = KafkaConsumer(
+     "map",
+     bootstrap_servers=ADDR_KAFKA,
+     auto_offset_reset='earliest',
+     enable_auto_commit=True,
+     group_id="al",
+     value_deserializer = lambda v: pickle.loads(v)
+    )
 
-    return res
+    for msg in consumer:
+        mapa = list(msg.value.values())[0]
 
 if __name__=="__main__":
-    npc = None
-    npcs = []
-    i = 0
-    superNPC = False # superNPC es la categoría
+    alias = "8NPC"
+    Thread(target=funcrecv, daemon=True).start()
+    t2 = Thread(target=funcsend, args=(alias,), daemon=True)
+    t2.start()
+    t2.join()
+    print("he salido de la pista")
+    time.sleep(10000)
 
-    # cada 10" crea un NPC cheto o random que se mueve cada [0,1] segundos
-    while True:
-        superNPC = not superNPC
-        npc = NPC("NPC"+str(i), superNPC)
-        npcs.append(npc)
-        i = i + 1
-        print("soy "+ npc.getAlias() + " y estoy chetísimo")
-        for n in npcs:
-            move(n,n.getCategory())
-            time.sleep(random.uniform(0, 1))
-        time.sleep(10)
